a88a69d8e59be1036b07ba040211d60c49fb900b
Izaura <izahonorato@gmail.com>
2022-07-22 20:13:00 -0300

    ajuste no endpoint de usuário para obter dados do usuário

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,9 +1,27 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {validaTokenJWT} from '../../middlewares/validaTokenJWT'
+import { conectaMongoDB } from '../../middlewares/conectaMongoDB'
+import { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg'
+import { UserModel } from '../../models/UserModel'
 
-const usuarioEndpoint = (req: NextApiRequest, res: NextApiResponse) => {
+const usuarioEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
 
-    return res.status(200).json('Usuário autenticado com sucesso.')
+    try{
+        //pegando o ID de usuário a partir do token
+        const {userId} = req?.query
+        //buscando o usuário pelo ID
+        const usuario = await UserModel.findById(userId)
+        usuario.senha = null
+        return res.status(200).json(usuario)
+    }catch(e){
+        console.log(e)
+        return res.status(400).json({error: 'Não foi possível obter dados do usuário.'})
+    }
+
+
+
+
+    return res.status(200).json({error: 'Usuário autenticado com sucesso.'})
 }
 
-export default validaTokenJWT(usuarioEndpoint)
+export default validaTokenJWT(conectaMongoDB(usuarioEndpoint))
