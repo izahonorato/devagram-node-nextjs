ef3da22b5e2f23856c21f0bf0aac301cc2afa33d
Izaura <izahonorato@gmail.com>
2022-07-22 20:26:15 -0300

    configurando endpoint de feed para trazer publicações de usuário específico

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -3,24 +3,35 @@ import {validaTokenJWT} from '../../middlewares/validaTokenJWT'
 import { conectaMongoDB } from '../../middlewares/conectaMongoDB'
 import { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg'
 import { UserModel } from '../../models/UserModel'
+import { PublicacaoModel } from '../../models/PublicacaoModel'
 
-const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
     try{
         if(req.method === 'GET'){
-            if(!req?.query?.id){
+            if(req?.query?.id){
+                //buscando usuário pelo ID
                 const usuario = await UserModel.findById(req?.query?.id)
 
+                //validando se o usuário existe
                 if(!usuario){
                     return res.status(400).json({error: 'Usuário não existe.'})
                 }
+                //buscando publicações do usuário
+                const publicacoes = await PublicacaoModel
+                    .find({idUsuario: usuario._id}) //busca todas as publicacoes desse ID
+                    .sort({data: -1}) //ordena por data, mais recente para mais antigo
+                    return res.status(200).json(publicacoes)
             }
+
+
         }
         return res.status(405).json({error: 'Método informado não é válido.'})
 
     }catch(e){
         console.log(e)
-        return res.status(400).json({error: 'Não foi possível obter o feed.'})
+        
     }
+    return res.status(400).json({error: 'Não foi possível obter o feed.'})
 }
 
 export default validaTokenJWT(conectaMongoDB(feedEndpoint))
